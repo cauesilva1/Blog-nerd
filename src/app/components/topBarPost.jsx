f468085d94a31412621f413b114c1cfdf5bed99d
Cauê catone silva <cauecatonesilva@gmail.com>
2023-10-08 01:12:10 -0400

    mudança na tela de home, adicionando uma tab de feed e my post's

---
+++ src/app/components/topBarPost.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React, { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+import { Button } from "../components/Button";
+
+export default function TopBarPost() {
+    const [activeTab, setActiveTab] = useState("feed"); // Inicialmente, definimos a guia "Feed" como ativa
+
+    return (
+        <div className="flex items-center justify-between w-[50%] mt-6 mb-3 max-[780px]:w-full max-[780px]:px-4">
+            <div className="flex items-center gap-9 text-[#6C757D] max-[780px]:gap-3">
+                <button
+                    className={activeTab === "feed" ? "text-[#246CD8]" : ""}
+                    onClick={() => setActiveTab("feed")}
+                >
+                    Feed
+                </button>
+
+                <button
+                    className={activeTab === "myposts" ? "text-[#246CD8]" : ""}
+                    onClick={() => setActiveTab("myposts")}
+                >
+                    My Post's
+                </button>
+            </div>
+
+            <Button.Root href="createpost" className="  bg-[#246CD8]">
+                <Button.Content>Criar post</Button.Content>
+                <IoMdAdd className="text-white"/>
+            </Button.Root> 
+        </div>
+    );
+}
+
+
+
